cca29cd7f1e060d896497486fe1119797b06dfee
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-04-06 13:45:51 -0400

    adds keypress func

--- script.js
+++ script.js
@@ -15290,14 +15290,18 @@ const dictionary = [
   "shave",
 ];
 
+const guessGrid = document.querySelector("[data-guess-grid]");
+
+startInteraction();
+
 function startInteraction() {
   document.addEventListener("click", handleMouseClick);
   document.addEventListener("keydown", handleKeyPress);
 }
 
 function stopInteraction() {
-    document.removeEventListener("click", handleMouseClick);
-    document.removeEventListener("keydown", handleKeyPress);
+  document.removeEventListener("click", handleMouseClick);
+  document.removeEventListener("keydown", handleKeyPress);
 }
 
 function handleMouseClick(e) {
@@ -15316,16 +15320,29 @@ function handleMouseClick(e) {
 }
 
 function handleKeyPress(e) {
-    if(e.key === "Enter"){
-        submitGuess();
-        return;
-    }
-    if(e.key === "Backspace" || e.key === "Delete"){
-        deleteKey();
-        return;
-    }
-    if(e.key.match(/^[a-z]$/)){
-        pressKey(e.key);
-        return;
-    }
+  if (e.key === "Enter") {
+    submitGuess();
+    return;
+  }
+  if (e.key === "Backspace" || e.key === "Delete") {
+    deleteKey();
+    return;
+  }
+  if (e.key.match(/^[a-z]$/)) {
+    pressKey(e.key);
+    return;
+  }
+}
+
+function pressKey(key) {
+  const activeTiles = getActiveTiles();
+  if (activeTiles.length >= 5) return;
+  const nextTile = guessGrid.querySelector(":not([data-letter])");
+  nextTile.dataset.letter = key.toLowerCase();
+  nextTile.textContent = key;
+  nextTile.dataset.state = "active";
+}
+
+function getActiveTiles() {
+  return guessGrid.querySelectorAll('[data-state="active"]');
 }
